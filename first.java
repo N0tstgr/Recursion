d9a6923bee71ea795a3afae722004d13733422bc
Notstranger <vikashkasaudhan321@gmail.com>
2026-07-27 23:23:59 +0530

    Add Main class with recursive message methods

---
+++ first.java
@@ -0,0 +1,34 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        message();
+    }
+    static void message(){
+        System.out.println("Hello");
+        message1();
+    }
+
+    static void message1(){
+        System.out.println("Hello");
+        message2();
+
+    }
+
+    static void message2(){
+        System.out.println("Hello");
+        message3();
+
+    }
+
+    static void message3(){
+        System.out.println("Hello");
+        message4();
+
+    }
+
+    static void message4(){
+        System.out.println("Hello");
+
+    }
+}
