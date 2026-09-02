2f7847bd778d568e2bee132b5cbe0e1b64d6a439
Notstranger <vikashkasaudhan321@gmail.com>
2026-09-02 13:52:40 +0530

    Add palindrome checking and reverse functions
    Implement palindrome checking and number reversal methods.

---
+++ palinincluded.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+// 		 reverse(4565);
+        System.out.println(palin(11));
+
+	}
+	 static int rev  = 0;
+    public static void reverse(int n){
+        int rem = 0;
+        if(n==0){
+            return;
+        }
+        rem = n%10;
+        rev = rev *10 + rem;
+          reverse(n/10);
+    }
+    public static int rev2(int n){
+        int digits = (int)(Math.log10(n) + 1);
+        return helper(n,digits);
+    }
+    private static int helper(int n, int digits){
+        if(n%10==n){
+            return n;
+        }
+        int rem = n%10;
+        return rem *(int)(Math.pow(10,digits-1)) + helper(n/10, digits-1);
+    }
+    static boolean palin(int n){
+        return (n==rev2(n));
+    }
+    
+}
