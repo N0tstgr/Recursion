967c6a2ff01c30a439cf8b2833bd565ab61e7bb7
Notstranger <vikashkasaudhan321@gmail.com>
2026-09-03 15:32:52 +0530

    Create kkapproach.java

---
+++ kkapproach.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int target = 1;
+	    int[] nums = {1,2,6,4,5};
+	    System.out.println(sh(nums, target, 0));
+	    
+         	}
+         	static boolean sh(int[] nums,  int target , int index){
+         	    if(index==nums.length){
+         	        return false;
+         	    }
+         	    return  (nums[index]== target || sh(nums, target, index+ 1));
+         	}
+}
