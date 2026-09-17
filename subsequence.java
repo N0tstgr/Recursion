47288a2d668875c7a728a6ec51a6cd335783f497
Notstranger <vikashkasaudhan321@gmail.com>
2026-09-17 18:35:35 +0530

    Add subsequence generation method
    Implement a recursive method to print all subsequences of a string.

---
+++ subsequence.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		subseq("", "abc");
+
+	}
+	 public static void subseq(String p, String up){
+	    if(up.isEmpty()){
+	        System.out.println(p);
+	        return;
+	    }
+	    
+	    char ch = up.charAt(0);
+	    subseq(p+ch, up.substring(1));
+	    subseq(p, up.substring(1));
+	}
+}
